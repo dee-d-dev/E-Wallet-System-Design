c2025fbf321f27c670e9999a0c47c5a07b691bea
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-28 07:03:43 +0100

    added a webhook for transfer status

--- wallet_code/app.js
+++ wallet_code/app.js
@@ -23,6 +23,33 @@ app.use("/api/v1", finalizeTransfer);
 app.use("/api/v1", register_user);
 app.use("/api/v1", login_user);
 
+app.post(
+  "https://web.hook.sh/#/9e087a6d-b602-4da3-838a-984481f824a6/5834caba-9566-4772-911a-18d95d121163",
+  function (req, res) {
+    // Retrieve the request's body
+    let event = req.body;
+    // Do something with event
+    res.send(200);
+  }
+);
+
+let crypto = require("crypto");
+
+// Using Express
+app.post("/my/webhook/url", function (req, res) {
+  //validate event
+  var hash = crypto
+    .createHmac("sha512", process.env.SECRET_KEY)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+  if (hash == req.headers["x-paystack-signature"]) {
+    // Retrieve the request's body
+    var event = req.body;
+    // Do something with event
+  }
+  res.send(200);
+});
+
 app.listen(5000, () => {
   console.log(`running on 5000`);
 });
