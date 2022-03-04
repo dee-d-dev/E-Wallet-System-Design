f717e82688c373e3f4ecc8d2f94da2873e59f1a8
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-04 20:28:33 +0100

    implemeting transfer via email

--- wallet_code/controllers/transfer_money.js
+++ wallet_code/controllers/transfer_money.js
@@ -2,10 +2,10 @@ const Wallet = require("../db/models/wallet");
 const User = require("../db/models/user");
 
 exports.transfer_money = (req, res) => {
-  const { sender_id, receiver_id, sender_wallet_id, receiver_wallet } =
+  const { sender_email, receiver_email, sender_wallet_id, receiver_wallet_id } =
     req.body;
-  const sender = Wallet.findById(sender_id);
-  const receiver = Wallet.findById(receiver_id);
+  const sender_email = User.findById(sender_email);
+  const receiver_email = User.findById(receiver_email);
 
   if (sender && receiver) {
     const sender_wallet = Wallet.findById(sender_wallet_id);
