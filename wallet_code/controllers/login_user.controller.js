dc5dbe5c3d3e4d23ad24837446a41cc2049c7ee0
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-05 16:56:48 +0100

    validation

--- wallet_code/controllers/login_user.controller.js
+++ wallet_code/controllers/login_user.controller.js
@@ -1,11 +1,26 @@
 const User = require("../db/models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const joi = require("joi");
 
 const login_user = async (req, res) => {
   const { email, password } = req.body;
   if (!(email && password)) {
     res.send("email and password is required");
+  }
+
+  const schema = joi.object({
+    username: joi.string().required(),
+    email: joi.string().email(),
+    password: joi.string().required(),
+  });
+
+  const validation = schema.validate({ username, email, password });
+  if (validation.error) {
+    return {
+      success: false,
+      error: validation.error.details[0].message,
+    };
   }
 
   const user = await User.findOne({ email });
--- wallet_code/controllers/transferWebHook.js
+++ wallet_code/controllers/transferWebHook.js
@@ -1,18 +1,18 @@
 const webHook = (req, res) => {
-    var crypto = require("crypto");
-    var secret = process.env.SECRET_KEY;
-    // Using Express
-    app.post("/my/webhook/url", function (req, res) {
-      //validate event
-      var hash = crypto
-        .createHmac("sha512", secret)
-        .update(JSON.stringify(req.body))
-        .digest("hex");
-      if (hash == req.headers["x-paystack-signature"]) {
-        // Retrieve the request's body
-        var event = req.body;
-        // Do something with event
-      }
-      res.send(200);
-    });
-}
+  var crypto = require("crypto");
+  var secret = process.env.SECRET_KEY;
+  // Using Express
+  app.post("/my/webhook/url", function (req, res) {
+    //validate event
+    var hash = crypto
+      .createHmac("sha512", secret)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+    if (hash == req.headers["x-paystack-signature"]) {
+      // Retrieve the request's body
+      var event = req.body;
+      // Do something with event
+    }
+    res.send(200);
+  });
+};
