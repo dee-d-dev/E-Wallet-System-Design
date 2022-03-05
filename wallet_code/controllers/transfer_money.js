5196932cbf391d7c74c40b1ada5dbd4681069988
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-05 14:21:48 +0100

    user can send to other user via email

--- wallet_code/controllers/transfer_money.js
+++ wallet_code/controllers/transfer_money.js
@@ -18,39 +18,27 @@ exports.transfer_money = async (req, res) => {
     const sender_wallet = await sender.wallet_id;
 
     const receiver_wallet = receiver.wallet_id;
-    // console.log(sender_wallet.balance)
-    // const sender_wallet = await Wallet.findById(sender_wallet_id).select(
-    //   "_id balance"
-    // );
 
-    // const receiver_wallet = await Wallet.findById(receiver_wallet_id).select(
-    //   "_id"
-    // );
-    // res.status(200).send({ sender: sender, receiver: receiver });
-
-    if (sender) {
+    if (amount < sender_wallet.balance) {
       //if amount is lesser than balance then send money
-      //   const receiver_balance = await Wallet.findByIdAndUpdate(
-      //     receiver_wallet,
-      //     {
-      //       $inc: { balance: amount },
-      //     },
-      //     { new: true }
-      //   );
-      //   const sender_balance = await Wallet.findByIdAndUpdate(
-      //     sender_wallet,
-      //     {
-      //       $inc: { balance: -amount },
-      //     },
-      //     { new: true }
-      //   );
+      const receiver_balance = await Wallet.findByIdAndUpdate(
+        receiver_wallet,
+        {
+          $inc: { balance: amount },
+        },
+        { new: true }
+      );
+      const sender_balance = await Wallet.findByIdAndUpdate(
+        sender_wallet,
+        {
+          $inc: { balance: -amount },
+        },
+        { new: true }
+      );
 
-      //   res.send(sender_wallet, receiver_wallet);
-      console.log(sender_wallet.balance);
-      res.send(sender_wallet.balance);
-      //   console.log(receiver_balance, sender_balance);
+      res.status(200).send({ success: true, amount: amount });
     } else {
-      res.send("cannot process action");
+      res.send({ success: false, message: "Insufficient balance" });
     }
   }
 };
