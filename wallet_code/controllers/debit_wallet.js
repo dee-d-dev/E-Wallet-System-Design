e7cf1461fb36b5833773962491f6fd04fbd8e7ac
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-06 17:38:43 +0100

    added password request to the debit endpoint

--- wallet_code/controllers/debit_wallet.js
+++ wallet_code/controllers/debit_wallet.js
@@ -1,15 +1,27 @@
 const Wallet = require("../db/models/wallet");
 const Transaction = require("../db/models/Transaction");
 const User = require("../db/models/User");
-const jwt_decode = require("jwt-decode");
-const jwt = require("jsonwebtoken");
-const { token } = require("../controllers/login_user.controller");
+const bcrypt = require("bcrypt");
+const joi = require("joi");
 
 exports.debit_wallet = async (req, res) => {
   const user = await User.findOne({ email: req.decoded.email });
-  const { amount, reason, recipient } = req.body;
+  const { amount, reason, password } = req.body;
   let wallet_id = user.wallet_id;
   const walletAmount = await Wallet.findById(wallet_id);
+  const schema = joi.object({
+    amount: joi.number().required(),
+    reason: joi.string().required(),
+    password: joi.string().required(),
+  });
+
+  const validation = schema.validate({ amount, reason, password });
+  if (!validation) return res.send("input fields not fully filled");
+
+  let authenticated = await bcrypt.compare(password, walletAmount.password);
+  if (!authenticated) {
+    res.send("wrong password");
+  }
 
   if (walletAmount.balance < amount) {
     return res.send({
