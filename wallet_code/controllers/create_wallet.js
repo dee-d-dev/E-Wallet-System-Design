65ca4db06a8c8156110e9014ac099e4c85ff9c87
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-31 23:55:30 +0100

    fixed

--- wallet_code/controllers/create_wallet.js
+++ wallet_code/controllers/create_wallet.js
@@ -5,8 +5,8 @@ const Wallet = require("../db/models/wallet");
 const create_wallet = async (req, res) => {
   const wallet = new Wallet(req.body);
   //   const user = new User(req.body);
-  const { user_id } = req.body;
-  const { balance } = req.body;
+  const { user_id, balance } = req.body;
+ 
 
   const user = User.findById(user_id);
 
